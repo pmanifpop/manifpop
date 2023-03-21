0968f6d2626268bbc1ab1b6a51cbb5ccb759585a
pmanifpop <128515958+pmanifpop@users.noreply.github.com>
2023-03-21 18:56:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 function validatePhone(value) {
+  console.log(value)
   var regex = /^0[1-9](\d{2}){4}$/;
   var result = true;
   if (value !== '') {
     result = regex.test(value);
   }
-
+  // publish to bubble
   bubble_fn_valid_phone(result)
 }
+
+function initValidatePhone() {
+  var phoneInput = document.querySelector('#phoneinput');
+  if (phoneInput) {
+    // inital state
+    validatePhone(phoneInput.value);
+    
+    // listen for changes
+    phoneInput.addEventListener('input', (e) => {
+      validatePhone(e.target.value)
+    });
+  }
+}
+
+function init() {
+  initValidatePhone();
+}
+
+init();
